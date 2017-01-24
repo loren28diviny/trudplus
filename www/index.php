b93844e2d1040dbf98f56f36dfcd614881ffd102
trud5 <trud5@ohranatruda5>
2017-01-24 16:38:30 +0300

    yhjhjhbnb

--- www/index.php
+++ www/index.php
@@ -1,19 +1,21 @@
-<?php iconv_set_encoding('output_encoding','ISO-8859-5')  ?>
 <!DOCTYPE html>
 <html>
 <head>
 <link href="style.css" rel="stylesheet">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
-<title>РћС…СЂР°РЅР° РўСЂСѓРґР°</title>
+<meta http-equiv="Content-Type"
+	content="text/html; charset=windows-1251" />
+<title>Охрана Труда</title>
 </head>
 <body>
-<div>
+<?php readfile("signin/form.html")?>
+<?php readfile("Menu/menu.html")?>
 <?php
-//setlocale(LC_ALL, 'ru_RU.UTF-8');
-include_once 'textstile.php';
-$TxT  = new TextGui("my", "style.css");
-$TxT->Text_echo();
+	//<div>
+// setlocale(LC_ALL, 'ru_RU.UTF-8');
+//include_once 'textstile.php';
+//$TxT = new TextGui ( "my", "style.css" );
+//$TxT->Text_echo ();
+//</div>
 ?>
-</div> 
 </body>
 </html>
