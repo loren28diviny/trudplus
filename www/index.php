005b4b19d80da309996bebd8da4584fe43515b2e
trud5 <trud5@ohranatruda5>
2017-01-25 16:49:54 +0300

    889jj

--- www/index.php
+++ www/index.php
@@ -5,18 +5,18 @@
 <head>
 <link href="style.css" rel="stylesheet">
 <meta http-equiv="Content-Type"
-	content="text/html; charset=utf-8" />
-<?php 
-setlocale(LC_ALL, 'ru_RU.UTF-8');
-?>
+		content="text/html; charset=utf-8" />
+		<?php
+		setlocale(LC_ALL, 'ru_RU.UTF-8');
+		?>
 <title>Охрана Труда</title>
 </head>
 <body>
 <body>
+
+<div class="back">
 <?php readfile("signin/form.html");?>
 <?php readfile("Menu/menu.html");?>
-
-<div>
 <?php 
 //include_once 'textstile.php';
 //$TxT = new TextGui ( "my", "style.css" );
---
+++ www/phpscript/constant.php
@@ -0,0 +1,3 @@
+<?php
+$size_display;
+?>
