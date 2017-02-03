170400b8c5e94b14088f8e22e55b32091e567370
loren28diviny <yahozayka@mail.ru>
2017-02-03 14:53:42 +0300

    js is doing

---
+++ www/contentAPI/script.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+function definition(o)
+{
+	
+  switch (true){
+   case (document.getElementById('po').value==o.value):
+       alert("lol");
+   break;
+   case (document.getElementById('ro').value==o.value) :
+	   alert("lol");
+   break;
+   case (document.getElementById('siz').value==o.value ):
+	   alert("lol");
+   break;
+   case (document.getElementById('lit').value==o.value) :
+	   alert("lol");
+   break;
+   case (document.getElementById('shv').value==o.value) :
+	   alert("lol");
+   break;
+   case (document.getElementById('drg').value==o.value) :
+	   alert("lol");
+   break;
+   default:
+	   alert("nelol");}
+	} 
